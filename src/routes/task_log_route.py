d2346607e52eba41b596bf4b8b2bdecd06843b81
Aman Ninave <amanninave@Amans-MacBook-Air.local>
2025-04-05 01:37:41 +0530

    task logs routes protection

--- src/routes/task_log_route.py
+++ src/routes/task_log_route.py
@@ -7,6 +7,8 @@ from typing import List
 from .. import database
 from ..schemas import task_log_schema
 from ..controllers import task_log_controller
+from ..models import models
+from ..utils.oauth2 import get_current_user
 
 router = APIRouter(
     prefix="/task-log",
@@ -16,29 +18,29 @@ router = APIRouter(
 get_db = database.get_db
 
 @router.post("/", response_model=task_log_schema.TaskLogResponse, status_code=status.HTTP_201_CREATED)
-def create(request: task_log_schema.TaskLogCreate, db: Session = Depends(get_db)):
+def create(request: task_log_schema.TaskLogCreate, db: Session = Depends(get_db), user: models.User = Depends(get_current_user)):
     return task_log_controller.create(db, request)
 
 @router.get("/{id}", response_model=task_log_schema.TaskLogResponse)
-def read(id: int, db: Session = Depends(get_db)):
+def read(id: int, db: Session = Depends(get_db), user: models.User = Depends(get_current_user)):
     log = task_log_controller.get(db, id)
     if not log:
         raise HTTPException(status_code=404, detail="Task Log not found")
     return log
 
 @router.get("/", response_model=list[task_log_schema.TaskLogResponse])
-def read(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def read(skip: int = 0, limit: int = 10, db: Session = Depends(get_db), user: models.User = Depends(get_current_user)):
     return task_log_controller.get_all(db, skip, limit)
 
 @router.put("/{id}", response_model=task_log_schema.TaskLogResponse)
-def update(id: int, request: task_log_schema.TaskLogUpdate, db: Session = Depends(get_db)):
+def update(id: int, request: task_log_schema.TaskLogUpdate, db: Session = Depends(get_db), user: models.User = Depends(get_current_user)):
     updated_log = task_log_controller.update(db, id, request)
     if not updated_log:
         raise HTTPException(status_code=404, detail="Task Log not found")
     return updated_log
 
 @router.delete("/{id}" , status_code=204)
-def delete(id: int, db: Session = Depends(get_db)):
+def delete(id: int, db: Session = Depends(get_db), user: models.User = Depends(get_current_user)):
     response = task_log_controller.delete(db, id)
     if not response:
         raise HTTPException(status_code=404, detail="Task Log not found")
