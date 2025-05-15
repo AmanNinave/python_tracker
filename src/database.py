5b5515e8122a34333db6dbe0e8918b01efee74af
Aman Ninave <amanninave@Amans-MacBook-Air.local>
2025-05-15 16:23:34 +0530

    migration to aws rds database

--- src/database.py
+++ src/database.py
@@ -1,12 +1,25 @@
 # Library imports
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'sqlite:///./tracker.db'
+# Load environment variables
+load_dotenv()
 
-engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={
-                       "check_same_thread": False})
+# Get database connection details from environment variables
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# Construct the database URL
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
--- src/models/models.py
+++ src/models/models.py
@@ -10,10 +10,10 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    email = Column(String)
-    password = Column(String)
-    # avatar = Column(String, nullable=True)  # Avatar URL or path
+    name = Column(String(255))
+    email = Column(String(255))
+    password = Column(String(255))
+    # avatar = Column(String(255), nullable=True)  # Avatar URL or path
     # settings = Column(JSON, nullable=True)  # Storing list as JSON ( color, icon, type)
     # privileges = Column(JSON, nullable=True)  # Storing list as JSON ( color, icon, type)
 
@@ -24,13 +24,13 @@ class Task(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
-    category = Column(String, nullable=False)   # Category of task (e.g., work, routine, personal)
-    sub_category = Column(String, nullable=False)    # Sub-category of task (e.g., sleep, frontend, backend)
+    category = Column(String(255), nullable=False)   # Category of task (e.g., work, routine, personal)
+    sub_category = Column(String(255), nullable=False)    # Sub-category of task (e.g., sleep, frontend, backend)
 
-    title = Column(String, nullable=True)  # Title of task
-    description = Column(String, nullable=True)    # Description of task ( Intigrate with markdown later)
+    title = Column(String(255), nullable=True)  # Title of task
+    description = Column(String(1000), nullable=True)    # Description of task ( Intigrate with markdown later)
 
-    status = Column(String, default='pending') # status :- pending, inprogress, completed
+    status = Column(String(255), default='pending') # status :- pending, inprogress, completed
     
     indicators = Column(JSON, nullable=True)  # Storing list as JSON ( remarks,rating, priority)
     settings = Column(JSON, nullable=True)  # Storing list as JSON ( color, icon, type)
@@ -47,7 +47,7 @@ class TaskSchedule(Base):
 
     start_time = Column(DateTime, nullable=False)   # Start time of the schedule entry
     end_time = Column(DateTime, nullable=False)  # End time of the schedule entry
-    remarks = Column(String, nullable=True)  # Remarks for the schedule entry
+    remarks = Column(String(1000), nullable=True)  # Remarks for the schedule entry
 
     task_id = Column(Integer, ForeignKey("tasks.id"))  # Foreign key to Tasks
 
@@ -62,7 +62,7 @@ class TaskLog(Base):
 
     start_time = Column(DateTime, nullable=False)   # Start time of the log entry
     end_time = Column(DateTime, nullable=True)  # End time of the log entry
-    remarks = Column(String, nullable=True)  # Remarks for the log entry
+    remarks = Column(String(1000), nullable=True)  # Remarks for the log entry
 
     task_schedule_id = Column(Integer, ForeignKey("task_schedule.id"))  # Foreign key to TaskSchedule
 
