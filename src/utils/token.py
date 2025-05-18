6fd695bcc6f64dd8ba63acc190b1ef30740a0ce8
Aman Ninave <amanninave@Amans-MacBook-Air.local>
2025-05-18 14:43:25 +0530

    updated token data

--- src/utils/token.py
+++ src/utils/token.py
@@ -1,4 +1,6 @@
 # Library imports
+import os
+from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
@@ -7,9 +9,12 @@ from sqlalchemy.orm import Session
 # from ..schemas import schema
 from ..models import models
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60*24
+# Load environment variables
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
