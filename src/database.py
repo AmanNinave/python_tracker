2ab2b35ebb67eb55ca7fbcec3d4ba2a3c59279d3
Aman Ninave <amanninave@Amans-MacBook-Air.local>
2025-05-18 18:57:11 +0530

    Database migration to free host

--- src/database.py
+++ src/database.py
@@ -9,17 +9,18 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 # Get database connection details from environment variables
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
 
-# Construct the database URL
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DB_USER = os.getenv("DB_USER")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
+# DB_HOST = os.getenv("DB_HOST")
+# DB_PORT = os.getenv("DB_PORT")
+# DB_NAME = os.getenv("DB_NAME")
+# # Construct the database URL
+# SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DB_CONNECTION_STRING)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
